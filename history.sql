e9dbe05a01bd4cb5d14ceeb9b1c0ab0922f96170
Patrick Kuo <patrick5267@gmail.com>
2016-06-20 15:54:27 +0800

    update sql history

--- history.sql
+++ history.sql
@@ -1,3 +1,6 @@
-update warehouse set id=id+100 where id=117;
+update warehouse set id=217 where id=117;
 update warehouse set id=117 where id=119;
 update warehouse set id=119 where id=217;
+update orders_item set item=217 where item=117;
+update orders_item set item=117 where item=119;
+update orders_item set item=119 where item=217;
